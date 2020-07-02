28e3153940aa8b11e7a4f7480a7c75da538ca618
sultana2020 <s2020sana@gmail.com>
2020-07-02 12:38:47 -0400

    my first testng project

---
+++ src/main/java/TestNGPracticeProject/TestNGPracticeProject/App.java
@@ -0,0 +1,13 @@
+package TestNGPracticeProject.TestNGPracticeProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
